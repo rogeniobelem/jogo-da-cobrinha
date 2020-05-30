2fb918e474769aed5e73b449b3c9912ccbfbf0a1
Rogênio Lima Belém <rogeniobelem@gmail.com>
2020-05-30 08:34:56 -0300

    Criar cobrinha

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("cobrinha");
 let context = canvas.getContext("2d");
 let box = 32;
+let cobrinha = [];
+cobrinha[0] = {
+    x: 8*box,
+    y: 8*box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha() {
+for(i=0; i<cobrinha.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
+}
+}
+
 criarBG();
+criarCobrinha();
